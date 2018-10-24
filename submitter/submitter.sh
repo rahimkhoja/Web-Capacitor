074901b6a180d185a9e609fa19f7388a785d71e1
Rahim Khoja <CanadianRepublican@users.noreply.github.com>
2018-10-24 12:04:32 -0700

    Update submitter.sh

--- submitter/submitter.sh
+++ submitter/submitter.sh
@@ -30,8 +30,57 @@ echo " You must have a sitemap.xml file within your website for this submitter t
 domain=freevoid.org
 subdomainlist=google-keywords.txt
 
+# Get SSL Domain Name
+finish="-1"
+while [ "$finish" = '-1' ]
+do
+    finish="1"
+    echo
+    read -p "Enter Domain to submit to Google & Bing [$domain]: " HOSTNAME
+    HOSTNAME=${HOSTNAME:-$domain}
+    echo
+    read -p "Domain: $HOSTNAME [y/n]? " answer
+
+    if [ "$answer" = '' ];
+    then
+        answer=""
+    else
+        case $answer in
+            y | Y | yes | YES ) answer="y";;
+            n | N | no | NO ) answer="n"; finish="-1"; exit 1;;
+            *) finish="-1";
+            echo -n 'Invalid Response\n';
+        esac
+    fi
+done
+
+
+
+finish="-1"
+while [ "$finish" = '-1' ]
+do
+    finish="1"
+    echo
+    read -p "Please enter subdomain list file [$subdomainlist]: " SUBLIST
+    SUBLIST=${SUBLIST:-$subdomainlist}
+    echo
+    read -p "Domain: $SUBLIST [y/n]? " answer
+
+    if [ "$answer" = '' ];
+    then
+        answer=""
+    else
+        case $answer in
+            y | Y | yes | YES ) answer="y";;
+            n | N | no | NO ) answer="n"; finish="-1"; exit 1;;
+            *) finish="-1";
+            echo -n 'Invalid Response\n';
+        esac
+    fi
+done
+
 cat $subdomainlist | \
 while read CMD; do
-    php submit.php http://$CMD.$domain/sitemap.xml
+    php submit.php http://$CMD.$HOSTNAME/sitemap.xml
     echo $CMD
 done
