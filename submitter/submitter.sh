dc3e69fd25a82bb892b6866670dc2f95c67a6f38
Rahim Khoja <CanadianRepublican@users.noreply.github.com>
2018-10-24 11:27:43 -0700

    Update submitter.sh

--- submitter/submitter.sh
+++ submitter/submitter.sh
@@ -1,3 +1,31 @@
+#!/bin/sh
+# Domain/Subdomain Submitter
+# By Rahim Khoja (rahim@khoja.ca)
+
+echo
+echo -e "\033[0;31m░░░░░░░░▀▀▀██████▄▄▄"
+echo "░░░░░░▄▄▄▄▄░░█████████▄ "
+echo "░░░░░▀▀▀▀█████▌░▀▐▄░▀▐█ "
+echo "░░░▀▀█████▄▄░▀██████▄██ "
+echo "░░░▀▄▄▄▄▄░░▀▀█▄▀█════█▀"
+echo "░░░░░░░░▀▀▀▄░░▀▀███░▀░░░░░░▄▄"
+echo "░░░░░▄███▀▀██▄████████▄░▄▀▀▀██▌"
+echo "░░░██▀▄▄▄██▀▄███▀▀▀▀████░░░░░▀█▄"
+echo "▄▀▀▀▄██▄▀▀▌█████████████░░░░▌▄▄▀"
+echo "▌░░░░▐▀████▐███████████▌"
+echo "▀▄░░▄▀░░░▀▀██████████▀"
+echo "░░▀▀░░░░░░▀▀█████████▀"
+echo "░░░░░░░░▄▄██▀██████▀█"
+echo "░░░░░░▄██▀░░░░░▀▀▀░░█"
+echo "░░░░░▄█░░░░░░░░░░░░░▐▌"
+echo "░▄▄▄▄█▌░░░░░░░░░░░░░░▀█▄▄▄▄▀▀▄"
+echo -e "▌░░░░░▐░░░░░░░░░░░░░░░░▀▀▄▄▄▀\033[0m"
+echo "---Domain & Subdomain submitter for Bing and Google---"
+echo "---By: Rahim Khoja (rahim.khoja@alumni.ubc.ca)---"
+echo
+echo " Please note, this script has only been tested with users who are under 5 foot 8 inches in height!"
+echo
+# Default Variables
 cat google-keywords.txt | \
 while read CMD; do
     php submit.php http://$CMD.wesupportford.ca/sitemap.xml
